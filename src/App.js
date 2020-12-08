ea455280fcbccb1b65024e1135f8227b15fa7193
Andriy Kikot <andray838@gmail.com>
2020-12-08 23:34:29 +0300

    added form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+// import { v4 as uuidv4 } from 'uuid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleNameChange = event => {
+    this.setState({ name: event.currentTarget.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form action="" onSubmit={this.handleSubmit}>
+          <label htmlFor="">
+            Name
+            <input
+              type="text"
+              value={this.state.name}
+              name="name"
+              onChange={this.handleNameChange}
+            />
+          </label>
+
+          <button type="submit">Add contact</button>
+
+          <label htmlFor="">
+            Number
+            <input type="text" />
+          </label>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
